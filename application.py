117b6c3d97ea9f39eb28b499b02007d7af26a33f
maryeleanor <marycasserly@gmail.com>
2020-05-06 16:41:33 -0700

    username login added

--- application.py
+++ application.py
@@ -1,28 +1,69 @@
 import os 
-
-from flask import Flask, render_template, url_for, jsonify
-from flask_socketio import SocketIO, emit
-
+import functools
+from flask import Flask, render_template, url_for, jsonify, request, session, redirect, flash
+from flask_socketio import SocketIO, emit, disconnect, join_room
+from flask_login import current_user, LoginManager, UserMixin, login_user, logout_user
+from flask_session import Session
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+socketio = SocketIO(app, manage_session=False)
+ 
+login_manager = LoginManager()
+login_manager.init_app(app)  
 
-chats = []
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    #if user clicked login
+    if request.method == "POST":
+
+        #get username from form
+        username = request.form.get("username")
+        
+        # ensure username was submitted
+        if not username: 
+            flash(u'Invalid credentials', 'error')
+            return render_template("register.html")
+ 
+        # remember which user has logged in
+        session["username"] = username
+        
+        # redirect user to home page
+        flash('You were successfully logged in.')
+        return render_template("index.html")
+
+    # user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 @socketio.on("submit chat")
 def chat(data):
     chat = data["message"]
-    print(chat)
-    emit("chat added", {"chat": chat}, broadcast=True)
+    username = session["username"]
+    print(f'{username}: {chat}') 
+    emit("chat added", {"chat": chat, "username": username}, broadcast=True)
+  
+@app.route("/account") 
+def account():
+     
+    # Redirect user to account page
+    return render_template("account.html")
 
 
-@socketio.on("submit vote")
-def vote(data):
-    selection = data["selection"]
-    emit("announce vote", {"selection": selection}, broadcast=True)
+@app.route("/logout") 
+def logout():
+    # Forget username
+    session.clear()
+
+    # Redirect user to homepage
+    return redirect("/")
+
+ 
--- static/index.js
+++ static/index.js
@@ -5,44 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    // When connected, configure buttons
+
     socket.on('connect', () => {
 
-        // Each button should emit a "submit vote" event
-        document.querySelectorAll('button').forEach(button => {
-            button.onclick = () => {
-                const selection = button.dataset.vote;
-                socket.emit('submit vote', { 'selection': selection });
+        input.addEventListener("keyup", function (event) {
+            // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                // Trigger the button element with a click
+                document.querySelector('#chat').click();
             };
         });
-    });
 
-    // When a new vote is announced, add to the unordered list
-    socket.on('announce vote', data => {
-        const li = document.createElement('li');
-        console.log(li)
-        li.innerHTML = `Vote recorded: ${data.selection}`;
-        document.querySelector('#votes').append(li);
-    });
-
-    //////
-
-    socket.on('connect', () => {
-        document.querySelector('#submit').onclick = () => {
+        document.querySelector('#chat').onclick = () => {
             input = document.querySelector('#input').value;
-            socket.emit('submit chat', {'message': input});
+            if (input.length > 0) {    
+                socket.emit('submit chat', {'message': input});
+                document.querySelector('#input').value = "";
+                return false;
+            };
         };
     });
 
     socket.on('chat added', data => {
         var p = document.createElement('p');
-        p.innerHTML = `${data.chat}`;
+        p.innerHTML = `${data.username}: ${data.chat}`;
         console.log(p);
         document.querySelector('#result').append(p);
     });
- 
-
-
+  
 
 });
  
