d3ebc37c47cf19d376d572483aa0311de2cfa549
maryeleanor <marycasserly@gmail.com>
2020-05-07 13:33:45 -0700

    updated styling of chat room and message scrolling

--- application.py
+++ application.py
@@ -2,22 +2,14 @@ import os
 import functools
 from collections import deque
 from flask import Flask, render_template, url_for, jsonify, request, session, redirect, flash
-from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room, close_room, rooms
-# from flask_login import current_user, LoginManager, UserMixin, login_user, logout_user
+from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room, close_room, rooms 
 from flask_session import Session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") 
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-socketio = SocketIO(app, manage_session=False)
- 
-# login_manager = LoginManager()
-# login_manager.init_app(app)  
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.get(user_id)
+socketio = SocketIO(app, manage_session=False) 
 
 home = deque(maxlen=100)
 
@@ -37,37 +29,29 @@ def index():
         # remember which user has logged in
         session["username"] = username
         
-        # redirect user to home page
+        # redirect user back to home page
         flash('You were successfully logged in.')
-        # home = None
-        # if 'home_room' in session:
-          #  home = session["home_room"]
-        return render_template("index.html", username = username, home = home)
+        home_room = list(home)
+        return render_template("index.html", username = username, home_room = home_room)
  
     # user reached route via GET (as by clicking a link or via redirect)
     else:
         username = None
         if 'username' in session:
             username = session["username"]
-
-        # home = None        
-        # if 'home_room' in session:
-          #  home = session["home_room"]
-
-        return render_template("index.html", username = username, home = home)
+        
+        home_room = list(home) 
+        return render_template("index.html", username = username, home_room = home_room)
 
 
 @socketio.on("submit chat")
 def chat(data):
     chat = data["message"]
     username = session["username"]
-    message = username + ": " + chat
-    print(message) 
+    message = username + ": " + chat 
 
-    home.append(message)
-    # session["home_room"] = home
-    home_room = list(home)
-    print(home) 
+    home.append(message) 
+    home_room = list(home) 
 
     emit("chat added", {"chat": message, "home": home_room}, broadcast=True)
   
@@ -90,9 +74,11 @@ def on_leave(data):
 
 @app.route("/account") 
 def account():
-     
+    if 'username' in session:
+        username = session["username"]
+
     # Redirect user to account page
-    return render_template("account.html")
+    return render_template("account.html", username=username)
 
 
 @app.route("/logout") 
--- static/index.js
+++ static/index.js
@@ -1,51 +1,58 @@
 
 document.addEventListener('DOMContentLoaded', () => {
- 
+
+    var messageBody = document.querySelector('#home_room');
+    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     socket.on('connect', () => {
 
+        input = document.querySelector('#chat');  
+
         input.addEventListener("keyup", function (event) {
             // Number 13 is the "Enter" key on the keyboard
             if (event.keyCode === 13) {
                 // Trigger the button element with a click
-                document.querySelector('#chat').click();
+                document.querySelector('#send_chat').click();
             };
         });
 
-        document.querySelector('#chat').onclick = () => {
-            input = document.querySelector('#input').value;
-            if (input.length > 0) {    
-                socket.emit('submit chat', {'message': input});
-                document.querySelector('#input').value = "";
+        document.querySelector('#send_chat').onclick = () => {
+            chat = document.querySelector('#chat').value;
+            if (chat.length > 0) {    
+                socket.emit('submit chat', {'message': chat});
+                document.querySelector('#chat').value = "";
                 return false;
             };
         };
-    });
+   
 
-    socket.on('chat added', data => {
-        var p = document.createElement('p');
-        p.innerHTML = `${data.chat}`;
-        console.log(p);
-        document.querySelector('#result').append(p);
-        if (!localStorage.getItem('home'))
-            localStorage.setItem('home', data.home);    
-    });
-    
+        socket.on('chat added', data => {
+            var p = document.createElement('p');
+            p.innerHTML = `${data.chat}`;
+            console.log(p);
+            document.querySelector('#home_room').append(p);
+            messageBody.scrollTop = messageBody.scrollHeight; 
+            if (!localStorage.getItem('home')) {
+                localStorage.setItem('home', data.home);    
+            };
+        });
+        
 
-    document.querySelector('#room2').onclick = () => {
-    socket.emit('create', 'room2');
+        document.querySelector('#room2').onclick = () => {
+        socket.emit('create', 'room2');
 
-    // server side code
-    io.sockets.on('connection', function(socket) {
-    socket.on('create', function(room2) {
-        socket.join(room2);
+            // server side code
+            io.sockets.on('connection', function(socket) {
+            socket.on('create', function(room2) {
+                socket.join(room2);
+                    });
             });
+            
+        };
+        
     });
-       
 
-
-});
 });
  
