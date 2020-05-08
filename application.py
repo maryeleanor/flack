0186e9f81ccbf595812f2665d226d27ec867dc42
maryeleanor <marycasserly@gmail.com>
2020-05-07 23:32:02 -0700

    added rooms with socketio, need to finish flask server login details for the room selection

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 import os 
 import functools
+from time import localtime, strftime
 from collections import deque
 from flask import Flask, render_template, url_for, jsonify, request, session, redirect, flash
-from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room, close_room, rooms 
+from flask_socketio import SocketIO, emit, disconnect, join_room, leave_room, close_room, rooms, send
 from flask_session import Session
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ Session(app)
 socketio = SocketIO(app, manage_session=False) 
 
 home = deque(maxlen=100)
+rooms = ["home", "news", "another room"]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,45 +32,65 @@ def index():
         session["username"] = username
         
         # redirect user back to home page
-        flash('You were successfully logged in.')
+        # flash('You were successfully logged in.')
         home_room = list(home)
-        return render_template("index.html", username = username, home_room = home_room)
+        room = 'home'
+        return render_template("index.html", username = username, rooms=rooms, room=room)
  
     # user reached route via GET (as by clicking a link or via redirect)
     else:
         username = None
         if 'username' in session:
             username = session["username"]
-        
-        home_room = list(home) 
-        return render_template("index.html", username = username, home_room = home_room)
+        room = 'home'
+        # home_room = list(home) 
+        return render_template("index.html", username = username, room = room, rooms=rooms)
 
 
-@socketio.on("submit chat")
-def chat(data):
-    chat = data["message"]
+@socketio.on('connect to room')
+def connection(data):
     username = session["username"]
-    message = username + ": " + chat 
+    timestamp = strftime('%b %d, %I:%M %p', localtime())
+    msg = data["msg"]
+    room = 'home'
+    # msg = time_stamp + username + msg
+    print(msg)
+    send({'msg': msg, 'username': username, 'timestamp': timestamp, 'room': room}, room=room, broadcast=True)
 
-    home.append(message) 
-    home_room = list(home) 
 
-    emit("chat added", {"chat": message, "home": home_room}, broadcast=True)
+@socketio.on('send chat')
+def chat(data):
+    username = session["username"]
+    chat = data["chat"]
+    room = data["room"]
+    timestamp = strftime('%b %d, %I:%M %p', localtime())
+    # message = username + ": " + chat 
+    # msg = data['message']
+    # home.append(message) 
+    # home_room = list(home) 
+    send({'chat': chat, 'username': username, 'timestamp': timestamp, 'room': room}, room=room)
   
+
 @socketio.on('join')
-def on_join(data):
+def join(data):
     username = session['username']
     room = data['room']
-    join_room(room2)
-    send(username + ' has entered the room.', room=room)
+    timestamp = strftime('%b %d, %I:%M %p', localtime())
+    chat = " has entered "
+    join_room(room)
+    # send({'msg': msg, 'username': username, 'timestamp': timestamp, "room":room}, room=room)
+    send({'sysmsg': chat, 'username': username, 'timestamp': timestamp, 'room': room}, room=room)
 
 
 @socketio.on('leave')
-def on_leave(data):
+def leave(data):
     username = data['username']
     room = data['room']
+    timestamp = strftime('%b %d, %I:%M %p', localtime())
+    chat = " has left "
     leave_room(room)
-    send(username + ' has left the room.', room=room)
+    #send({'msg': msg, 'username': username, 'timestamp': timestamp, "room":room}, room=room)
+    send({'sysmsg': chat, 'username': username, 'timestamp': timestamp, 'room': room}, room=room)
 
 
 
--- static/index.js
+++ static/index.js
@@ -1,58 +1,104 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // let room = localStorage.getItem('room');  
+    // let username = localStorage.getItem('username');
+    // console.log(username)
+    // console.log(room)
 
-    var messageBody = document.querySelector('#home_room');
+    // scroll chatroom to bottom in case there's a lot of messages
+    var messageBody = document.querySelector('#chatroom');
     messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
+
+    // trigger chat button click with enter key 
+    input = document.querySelector('#chat');  
+    input.addEventListener("keyup", event => {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) { 
+            document.querySelector('#send_chat').click();
+        };
+    });
+
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    socket.on('connect', () => {
 
-        input = document.querySelector('#chat');  
-
-        input.addEventListener("keyup", function (event) {
-            // Number 13 is the "Enter" key on the keyboard
-            if (event.keyCode === 13) {
-                // Trigger the button element with a click
-                document.querySelector('#send_chat').click();
-            };
+        // on connect, send message to server
+        socket.on('connect', () => {
+            room = localStorage.getItem('room');
+            console.log(room);  
+            socket.emit('connect to room', {'msg': ' has joined ', 'room': room}, room=room);
         });
 
+        // listen for connect message from server and append to chatroom
+        socket.on('message', data => {
+            console.log(data.room);
+            console.log(data.msg);
+            console.log(data.username)
+            // console.log(data.sysmsg);
+
+            var span_date = document.createElement('span');
+            span_date.classList.add("date");
+            var span_username = document.createElement('span');
+            span_username.classList.add("username");
+            var p = document.createElement('p');
+            var chat_space = ": ";
+            span_date.innerHTML = data.timestamp;
+            span_username.innerHTML = data.username;
+           
+            if (data.msg) {    
+                p.innerHTML = span_date.outerHTML  + span_username.outerHTML + data.msg + data.room;
+            }
+            else if (data.chat) {
+                p.innerHTML = span_date.outerHTML  + span_username.outerHTML + chat_space + data.chat;
+            }
+            else {
+                p.innerHTML = span_date.outerHTML  + span_username.outerHTML + data.sysmsg + data.room;;
+            }
+            messageBody.append(p);
+            messageBody.scrollTop = messageBody.scrollHeight;
+        });
+
+        // when chat send button clicked
         document.querySelector('#send_chat').onclick = () => {
             chat = document.querySelector('#chat').value;
+            room = localStorage.getItem('room');  
             if (chat.length > 0) {    
-                socket.emit('submit chat', {'message': chat});
-                document.querySelector('#chat').value = "";
+                socket.emit('send chat', {'chat': chat, 'room': room});
+                document.querySelector('#chat').value = " ";
                 return false;
             };
         };
-   
-
-        socket.on('chat added', data => {
-            var p = document.createElement('p');
-            p.innerHTML = `${data.chat}`;
-            console.log(p);
-            document.querySelector('#home_room').append(p);
-            messageBody.scrollTop = messageBody.scrollHeight; 
-            if (!localStorage.getItem('home')) {
-                localStorage.setItem('home', data.home);    
-            };
+        
+        // when room button clicked, get room name, leave old and join new
+        document.querySelectorAll('.select_room').forEach(li => {
+            li.onclick = () =>{
+                let newRoom = li.innerHTML;
+                if (newRoom == room) {
+                    msg = `You're already in ${room} room`;
+                    var p = document.createElement('p');
+                    p.innerHTML = msg;
+                    messageBody.append(p);
+                } else {
+                    socket.emit('leave', {'username': username, 'room': room});
+                    socket.emit('join', {'username': username, 'room': newRoom});
+                    localStorage.setItem('room', newRoom); 
+                    room = newRoom;
+                }
+            }
         });
         
+        // // emit leave room to server
+        // function leaveRoom(room){
+        //     socket.emit('leave', {'username': username, 'room': room});
+        // };
 
-        document.querySelector('#room2').onclick = () => {
-        socket.emit('create', 'room2');
+        // // emit join room to server
+        // function joinRoom(newRoom){
+        //     socket.emit('join', {'username': username, 'room': newRoom});
+        //     localStorage.setItem('room', newRoom); 
+        // };
 
-            // server side code
-            io.sockets.on('connection', function(socket) {
-            socket.on('create', function(room2) {
-                socket.join(room2);
-                    });
-            });
-            
-        };
-        
-    });
 
 });
  
